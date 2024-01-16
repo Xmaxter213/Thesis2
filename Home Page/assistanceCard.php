42d3e60c18f10ad69337b03c1481a26e04805b9c
[Andrei Sibucao] <reiandsibucao@gmail.com>
2024-01-16 23:35:35 +0800

    Rectified and enhanced the assistance card design, and made it responsive at lower resolutions.

--- Home Page/assistanceCard.php
+++ Home Page/assistanceCard.php
@@ -1,35 +1,33 @@
 <?php
 #bg-primary blue, bg-warning yellow, bg-success green, bg-danger red
-function assistanceCard($patient_ID, $patient_Name, $room_Number, $age, $admission_Status, $nurse_Name, $assistance_Status, $device_Assigned){
-    if ($assistance_Status == "Unassigned")
-    {
-        $assistance_Color = "bg-danger";
-    }
-    else if ($assistance_Status == "On the way")
-    {
-        $assistance_Color = "bg-primary";
-    } else
-    {
-        $assistance_Color = NULL;
-    }
+function assistanceCard($patient_ID, $patient_Name, $room_Number, $age, $admission_Status, $nurse_ID, $assistance_Status, $gloves_ID)
+{
+    $cardClasses = $assistance_Status == "Unassigned" ? "bg-danger" : "bg-info";
+    $bgClasses = $assistance_Status == "Unassigned" ? "bg-danger" : "bg-primary";
+    $btnClasses = $assistance_Status == "Unassigned" ? "btn-danger" : "btn-primary";
 
-    $element ="<div class=\"col-xl-3 col-md-6\">
-                                <div class=\"card $assistance_Color text-white mb-4\">
-                                    <div class=\"card-body\">Patient Name: $patient_Name</div>
-                                    <div class=\"card-body\">Room #: $room_Number</div>
-                                    <form action=\"index.php\">
-                                        <label style = \"margin-left: 15px\" for=\"remark\">Remark: </label>
-                                        <input type=\"text\" id=\"remark\" name=\"remark\"><br><br>
-                                        <input style = \"margin-left: 40%\" type=\"submit\" value=\"Submit\">
-                                    </form>
-
-                                    <div class=\"card-footer d-flex align-items-center justify-content-between\">
-                                        <a class=\"small text-white\" href=\"#\">View Details</a>
-                                        <div class=\"small text-white\"><i class=\"fas fa-angle-right\"></i></div>
-                                    </div>
-                                </div>
-                            </div>
+    $element = "
+    <div class=\"col-xl-3 col-md-6\">
+        <div class=\"card px-0 text-black $cardClasses bg-opacity-25\" style=\"width: 25rem;\">
+            <img src=\"./Images/room.jpg\" class=\"card-img-top\" alt=\"...\">
+            <div class=\"card-body\">
+                <h5 class=\"card-title\">Patient Name: <span class=\"fw-normal\">$patient_Name</span> <span class=\"badge $bgClasses\">$assistance_Status</span></h5>
+                <h5 class=\"card-title\">Room #: $room_Number</h5>
+                <form action=\"index.php\">
+                    <div class=\"d-flex align-items-center justify-content-center\">
+                        <h5 class=\"me-2 mb-0\">Remarks: </h5>
+                        <div class=\"input-group\">
+                            <input type=\"text\" class=\"form-control\" placeholder=\"Enter Remarks\" aria-describedby=\"button-addon\">
+                            <button class=\"btn $btnClasses\" type=\"submit\" id=\"button-addon\">Submit</button>
+                        </div>
+                    </div>
+                </form>
+                <br>
+                <a href=\"#\" class=\"btn $btnClasses\">View Details</a>
+            </div>
+        </div>
+    </div>
     ";
-    
+
     echo $element;
 }

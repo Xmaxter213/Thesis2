d6f635a646704192a6a1c7376b96888c72bab2cc
[Andrei Sibucao] <reiandsibucao@gmail.com>
2024-01-25 16:20:54 +0800

    Added Forgot Password Page

--- MainHospital/Login_new.php
+++ MainHospital/Login_new.php
@@ -1,10 +1,9 @@
-<?php 
-	require_once('../dbConnection/connection.php');
+<?php
+require_once('../dbConnection/connection.php');
 
-	if(isset($_SESSION['userID']))
-	{
-		header("Location: ../dumHomePage/index.php");
-	}
+if (isset($_SESSION['userID'])) {
+    header("Location: ../dumHomePage/index.php");
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,9 +22,7 @@
     <link rel="icon" type="image/x-icon" href="assets/FinalLogo.png" />
     <!-- Custom fonts for this template-->
     <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="css/sb-admin-2.min.css" rel="stylesheet">
@@ -53,22 +50,21 @@
                                     </div>
                                     <form class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user"
-                                                id="email" aria-describedby="emailHelp"
-                                                placeholder="Enter Email Address..." required>
+                                            <input type="email" class="form-control form-control-user" id="email" aria-describedby="emailHelp" placeholder="Enter Email Address..." required>
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user"
-                                                id="password" placeholder="Password" required>
+                                            <input type="password" class="form-control form-control-user" id="password" placeholder="Password" required>
                                         </div>
-                                        
+
                                         <button type="button" name="button" class="btn btn-primary btn-user btn-block" id="login">Login</button>
-                                        
+
                                     </form>
                                     <hr>
                                     <div class="text-center">
                                     </div>
                                     <div class="text-center">
+                                        <a class="small" href="./forgot_password.php">Forgot Password?</a>
+                                        <br>
                                         <a class="small" href="../Register Page/Register.php">Create an Account!</a>
                                         <br>
                                         <a class="small" href="../Nurses Station Page/Assistance Card Page/assistanceCard.php">Go to Home!</a>
@@ -89,81 +85,74 @@
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script>
     <script type="text/javascript">
-	$(function()
-		{
-			$('#login').click(function(e){
-				var valid = this.form.checkValidity();
+        $(function() {
+            $('#login').click(function(e) {
+                var valid = this.form.checkValidity();
 
-				if(valid)
-				{
+                if (valid) {
 
-					var email = $('#email').val();
-					var password = $('#password').val();
+                    var email = $('#email').val();
+                    var password = $('#password').val();
 
                     e.preventDefault();
 
                     $.ajax({
-                    type: 'POST',
-                    url: 'jslogin.php',
-                    data: {email: email, password: password},
-                    success: function(data){
-                        if(data === "Successfully")
-                        {
-                            Swal.fire({
-                                        'title': 'Successful',
-                                        'text': data,
-                                        'type': 'success'
-                                        })
-                            if($.trim(data) === "Successfully")
-                            {
-                                setTimeout('window.location.href = "../dumHomePage/index.php"', 2000);
-                            }
-                        }
-                        else if(data === "Account Not validated")
-                        {
-                            Swal.fire({
-                                        'title': 'Errors',
-                                        'text': data,
-                                        'type': 'error'
-                                        }).then(function(){
-                                                window.location = "../OTP/OTP.php";
-                                        });
-                        }
-                        else
-                        {
-                            Swal.fire({
-                                        'title': 'Errors',
-                                        'text': data,
-                                        'type': 'error'
-                                        })
-
-                        }
-                            
-                            },
-                            error: function(data){
+                        type: 'POST',
+                        url: 'jslogin.php',
+                        data: {
+                            email: email,
+                            password: password
+                        },
+                        success: function(data) {
+                            if (data === "Successfully") {
                                 Swal.fire({
-                                        'title': 'Errors',
-                                        'text': 'There were errors while saving the data.',
-                                        'type': 'error'
-                                        })
-                            }
-                            
-                })
+                                    'title': 'Successful',
+                                    'text': data,
+                                    'type': 'success'
+                                })
+                                if ($.trim(data) === "Successfully") {
+                                    setTimeout('window.location.href = "../dumHomePage/index.php"', 2000);
+                                }
+                            } else if (data === "Account Not validated") {
+                                Swal.fire({
+                                    'title': 'Errors',
+                                    'text': data,
+                                    'type': 'error'
+                                }).then(function() {
+                                    window.location = "../OTP/OTP.php";
+                                });
+                            } else {
+                                Swal.fire({
+                                    'title': 'Errors',
+                                    'text': data,
+                                    'type': 'error'
+                                })
 
-				}
-                else
-                {
+                            }
+
+                        },
+                        error: function(data) {
+                            Swal.fire({
+                                'title': 'Errors',
+                                'text': 'There were errors while saving the data.',
+                                'type': 'error'
+                            })
+                        }
+
+                    })
+
+                } else {
                     Swal.fire({
-                                        'title': 'Errors',
-                                        'text': 'Missing Informations',
-                                        'type': 'error'
-                                        })
+                        'title': 'Errors',
+                        'text': 'Missing Informations',
+                        'type': 'error'
+                    })
                 }
 
-				
-			})
+
+            })
         });
-</script>
+    </script>
 
 </body>
 
---
+++ MainHospital/forgot_password.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Recovery</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <style>
+        body{
+            background-color: rgb(78,115,223);
+        }
+    </style>
+</head>
+
+<body class="bg-gradient-primary">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden my-5">
+                    <div class="border-0 d-flex justify-content-center" style="background-color: #BFCFE7;">
+                        <div class="col-lg-6">
+                            <div class="p-5">
+                                <main class="form-signin">
+                                    <form>
+                                        <!-- <img class="mb-4" src="/docs/5.1/assets/brand/bootstrap-logo.svg" alt="" width="72" height="57"> -->
+                                        <div class="text-center">
+                                        <h1 class="h3 mb-3 fw-normal">Recover Password</h1>
+                                        <figcaption class="blockquote-footer" style="color:black">
+                                                Please enter your email address so we can assist you in recovering your account
+                                        </figcaption>
+                                        </div>
+
+                                        <div class="form-floating">
+                                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                            <label for="floatingInput">Email address</label>
+                                        </div>
+                                        <br>
+                                        <button class="w-100 btn btn-lg btn btn-outline-primary" type="submit" name="submit" id="" >Recover my Password</button>
+                                    </form>
+                                </main>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+</body>
+
+</html>
